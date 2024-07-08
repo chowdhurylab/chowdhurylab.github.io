d9058948f7f051407326d367dfd32cf2b6e45d68
YeeChuen <tychuen98@gmail.com>
2024-07-08 12:43:47 -0500

    resolve styling issue and minor bugs in mobile view for carousel with thumbnail

--- assets/js/carouselThumbnail.js
+++ assets/js/carouselThumbnail.js
@@ -1,74 +1,101 @@
 // JS to handle auto carousel
-window.onload = function () { 
-    const slides = document.querySelector(".carousel-slides").children;
-    const thumbnailsContainer = document.querySelector(".carousel-thumbnail").children;
+window.onload = function () {
+  const slides = document.querySelector(".carousel-slides").children;
+  const thumbnailsContainer = document.querySelector(".carousel-thumbnail");
+  const thumbnails = thumbnailsContainer.children;
 
-    if (slides.length !== thumbnailsContainer.length) throw Error("Mismatch number of slides and thumbnails.")
+  if (slides.length !== thumbnails.length)
+    throw Error("Mismatch number of slides and thumbnails.");
 
-    function addActive(itemElem) { 
-        itemElem.setAttribute("slide-active", "");
-    } 
-  
-    function removeActive(itemElem) { 
-        itemElem.removeAttribute("slide-active");
+  function addActive(itemElem) {
+    itemElem.setAttribute("slide-active", "");
+  }
+
+  function removeActive(itemElem) {
+    itemElem.removeAttribute("slide-active");
+  }
+
+  setInterval(function () {
+    for (let i = 0; i < slides.length; i++) {
+      if (slides[i].hasAttribute("slide-active")) {
+        if (i + 1 >= slides.length) {
+          // when slide is the last
+          setTimeout(removeActive(slides[i]), 350);
+          setTimeout(removeActive(thumbnails[i]), 350);
+          addActive(slides[0]);
+          addActive(thumbnails[0]);
+
+          const distance =
+          thumbnails[i].getBoundingClientRect().left -
+          thumbnails[0].getBoundingClientRect().left;
+          thumbnailsContainer.scrollLeft -= Math.abs(distance);
+          break;
+        } else {
+          setTimeout(removeActive(slides[i]), 350);
+          setTimeout(removeActive(thumbnails[i]), 350);
+          addActive(slides[i + 1]);
+          addActive(thumbnails[i + 1]);
+
+          const distance =
+          thumbnails[i].getBoundingClientRect().left -
+          thumbnails[i + 1].getBoundingClientRect().left;
+          thumbnailsContainer.scrollLeft += Math.abs(distance);
+          break;
+        }
+      }
     }
-
-    setInterval(function () { 
-        for (let i = 0; i < slides.length; i++) { 
-            if (slides[i].hasAttribute("slide-active")) {
-                if (i + 1 >= slides.length) {
-                    setTimeout(removeActive(slides[i]), 350);
-                    setTimeout(removeActive(thumbnailsContainer[i]), 350);
-                    addActive(slides[0]); 
-                    addActive(thumbnailsContainer[0]); 
-                    break;
-                    
-                } else {
-                    setTimeout(removeActive(slides[i]), 350); 
-                    setTimeout(removeActive(thumbnailsContainer[i]), 350); 
-                    addActive(slides[i + 1]);
-                    addActive(thumbnailsContainer[i + 1]);
-                    break;
-                }
-            }
-        } 
-    }, 3000); 
+  }, 3000);
 };
 
 // Javscript handling button left and right.
-const buttons = document.querySelectorAll(".carousel-button")
+const buttons = document.querySelectorAll(".carousel-button");
 
 buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-        const offset = (button.classList[1] === "next") ? 1 : -1;
+  button.addEventListener("click", () => {
+    const offset = button.classList[1] === "next" ? 1 : -1;
 
-        const slidesContainer = document.querySelector(".carousel-slides")
-        const thumbnailsContainer = document.querySelector(".carousel-thumbnail")
+    const slidesContainer = document.querySelector(".carousel-slides");
+    const thumbnailsContainer = document.querySelector(".carousel-thumbnail");
+    const activeSlides = document.querySelectorAll("[slide-active]");
 
-        const activeSlides = document.querySelectorAll("[slide-active]")
-        activeSlides.forEach((activeSlide) => activeSlide.removeAttribute("slide-active"))
+    const oldIndex = [...slidesContainer.children].indexOf(activeSlides[0]);
+    let newIndex = oldIndex + offset;
+    if (newIndex < 0) return;
+    if (newIndex >= slidesContainer.children.length) return;
 
-        let newIndex = [...slidesContainer.children].indexOf(activeSlides[0]) + offset;
-        if (newIndex < 0) newIndex = slidesContainer.children.length - 1;
-        if (newIndex >= slidesContainer.children.length) newIndex = 0;
+    activeSlides.forEach((activeSlide) =>
+      activeSlide.removeAttribute("slide-active")
+    );
 
-        // activeSlide.removeAttribute("slide-active");
-        slidesContainer.children[newIndex].setAttribute("slide-active", "");
-        thumbnailsContainer.children[newIndex].setAttribute("slide-active", "");
-    })
-})
+    // activeSlide.removeAttribute("slide-active");
+    slidesContainer.children[newIndex].setAttribute("slide-active", "");
+    thumbnailsContainer.children[newIndex].setAttribute("slide-active", "");
+
+    const distance =
+      thumbnailsContainer.children[newIndex].getBoundingClientRect().left -
+      thumbnailsContainer.children[oldIndex].getBoundingClientRect().left;
+
+    if (button.classList[1] === "next") {
+      thumbnailsContainer.scrollLeft += Math.abs(distance);
+    } else {
+      thumbnailsContainer.scrollLeft -= Math.abs(distance);
+    }
+  });
+});
 
 // thumbnail onclick
 const thumbnails = document.querySelectorAll(".carousel-thumbnail-item");
 thumbnails.forEach((thumbnail, i) => {
-    thumbnail.addEventListener("click", () => {
-        const slidesContainer = document.querySelector(".carousel-slides")
-        const thumbnailsContainer = document.querySelector(".carousel-thumbnail")
+  thumbnail.addEventListener("click", () => {
+    const slidesContainer = document.querySelector(".carousel-slides");
+    const thumbnailsContainer = document.querySelector(".carousel-thumbnail");
 
-        const activeSlides = document.querySelectorAll("[slide-active]")
-        activeSlides.forEach((activeSlide) => activeSlide.removeAttribute("slide-active"))
+    const activeSlides = document.querySelectorAll("[slide-active]");
+    activeSlides.forEach((activeSlide) =>
+      activeSlide.removeAttribute("slide-active")
+    );
 
-        slidesContainer.children[i].setAttribute("slide-active", "");
-        thumbnailsContainer.children[i].setAttribute("slide-active", "");
-    })
-})
+    slidesContainer.children[i].setAttribute("slide-active", "");
+    thumbnailsContainer.children[i].setAttribute("slide-active", "");
+  });
+});
