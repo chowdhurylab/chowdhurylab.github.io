a4bd29970074e6a636b47796bf342d7d3895a256
Riza Danurdoro <136944434+rzpd@users.noreply.github.com>
2026-03-26 14:50:42 -0500

    Update homepage layout and add external partner logos

--- assets/js/home-component.js
+++ assets/js/home-component.js
@@ -45,6 +45,15 @@
     var affiliationBody = buildAffiliations(hero);
     var affiliationPrefix = hero.affiliationsText ? hero.affiliationsText + ' ' : '';
     var affiliationSuffix = hero.affiliationsSuffix || '';
+    var externalLogos = [
+      { src: 'images/us-department-of-energy.png', alt: 'U.S. Department of Energy', href: 'https://www.energy.gov/' },
+      { src: 'images/national-science-foundation.png', alt: 'National Science Foundation', href: 'https://www.nsf.gov/' },
+      { src: 'images/global-methane-hub.png', alt: 'Global Methane Hub', href: 'https://globalmethanehub.org/' },
+      { src: 'images/iowa-economic-development.png', alt: 'Iowa Economic Development', href: 'https://iowaeda.com/' }
+    ];
+    var externalLogosMarkup = externalLogos.map(function (logo) {
+      return '<a class="home-external-logo-link" href="' + logo.href + '" target="_blank" rel="noopener noreferrer" aria-label="' + logo.alt + '"><img class="home-external-logo" src="' + logo.src + '" alt="' + logo.alt + '" loading="lazy" /></a>';
+    }).join('');
 
     mount.innerHTML = '' +
       '<header>' +
@@ -55,7 +64,8 @@
       '<p class="home-hero-affiliations">' + affiliationPrefix + affiliationBody + affiliationSuffix + '</p>' +
       '<ul class="actions">' +
       '  <li><a href="' + (cta.href || '#') + '" class="button big">' + (cta.label || 'Learn more') + '</a></li>' +
-      '</ul>';
+      '</ul>' +
+      '<div class="home-hero-external"><span class="home-hero-external-label">External:</span><div class="home-hero-external-logos">' + externalLogosMarkup + '</div></div>';
 
     imageMount.innerHTML = '<img src="' + (image.src || '') + '" alt="' + (image.alt || '') + '" />';
   }
